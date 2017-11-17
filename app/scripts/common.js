816ee4c705fb93f23a05803ba681549e34b6eb5a
Volodymyr Chygura <volodymyr.chygura@gmail.com>
2017-11-17 12:05:04 +0200

    added new block

--- app/scripts/common.js
+++ app/scripts/common.js
@@ -1,3 +1,25 @@
+function Header(selector) {
+    var h = $(this);
+
+    h.head = $(selector);
+
+    h.changeHeader = function () {
+        if ($("html").scrollTop() > 200) {
+            h.head.css({
+                "background": "rgba(0,0,0, .85)",
+                "padding": "10px 0"
+            })
+        } else {
+            h.head.css({
+                "background": "transparent",
+                "padding": "40px 0"
+            })
+        }
+    };
+
+    h.changeHeader();
+    $(window).scroll(h.changeHeader);
+}
 function Login(selector) {
     var l = $(this);
 
@@ -19,8 +41,33 @@ function Login(selector) {
 
     l.showLoginModuleBtn.click(l.showLoginModule);
 }
+function TitleSlides(selector) {
+    var ts = $(this);
+
+    ts.slides = $(selector);
+    ts.slide = ts.slides.find(".title-block-slide");
+
+    ts.changeSlide = function () {
+        ts.activeSlide = ts.slides.find(".title-block-slide--active");
+        ts.activeSlide.fadeOut(3000);
+        ts.activeSlide.removeClass("title-block-slide--active");
+        if (ts.activeSlide.next().length !== 0) {
+            ts.activeSlide.next().fadeIn(3000);
+            ts.activeSlide.next().addClass("title-block-slide--active");
+        } else {
+            ts.slide.first().fadeIn(3000);
+            ts.slide.first().addClass("title-block-slide--active");
+        }
+    };
+
+    setInterval(ts.changeSlide, 6000);
+}
+
 
 $(function () {
     // todo: create js-logic!!!
-    var login = new Login(".login");
+    var login = new Login(".login"),
+        header = new Header(".header"),
+        titleSlides = new TitleSlides(".title-block-slides")
+    ;
 });
